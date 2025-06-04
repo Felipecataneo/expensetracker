a5ccd27cc9e7c23aa003347b1222811b8310d07b
felipecataneo <felipecataneo@hotmail.com>
2025-06-03 21:11:24 -0300

    alterado amplifyconfig

--- src/amplifyconfig.ts
+++ src/amplifyconfig.ts
@@ -7,15 +7,15 @@ export const configureAmplify = () => {
       Cognito: {
         userPoolId: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID!,
         userPoolClientId: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID!,
-        region: process.env.NEXT_PUBLIC_AWS_REGION!,
+        
         // Exemplo para usar o Hosted UI (se você decidisse usar)
         // loginWith: {
         //   oauth: {
         //     domain: 'YOUR_COGNITO_DOMAIN.auth.us-east-1.amazoncognito.com',
         //     scopes: ['email', 'openid', 'profile'],
-        //     redirectSignIn: ['http://localhost:3000/'], // URL de callback
-        //     redirectSignOut: ['http://localhost:3000/auth'], // URL de logout
-        //     responseType: 'code' // ou 'token'
+        //     redirectSignIn: ['http://localhost:3000/'],
+        //     redirectSignOut: ['http://localhost:3000/auth'],
+        //     responseType: 'code'
         //   }
         // }
       },
